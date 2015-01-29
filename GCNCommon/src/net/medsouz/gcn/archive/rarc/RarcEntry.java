40a6cf6977f5cff6cc00085140ab8e026e3dd37b
Felix Kaaman <trundmatu@gmail.com>
2015-01-29 15:52:52 +0100

    Add basic support for extracting RARC archives

---
+++ GCNCommon/src/net/medsouz/gcn/archive/rarc/RarcEntry.java
@@ -0,0 +1,33 @@
+package net.medsouz.gcn.archive.rarc;
+
+import java.nio.ByteBuffer;
+
+public class RarcEntry {
+  public static final int SIZE = 20;
+
+  public short id;
+  public short hash;
+  public short type;
+  public short nameOffset;
+  public int fileOffset;
+  public int fileSize;
+
+  public String name;
+
+  public void read(ByteBuffer buf, ByteBuffer stringTable) {
+    this.id = buf.getShort();
+    this.hash = buf.getShort();
+    this.type = buf.getShort();
+    this.nameOffset = buf.getShort();
+    this.fileOffset = buf.getInt();
+    this.fileSize = buf.getInt();
+    buf.getInt(); // unused
+
+    this.name = RarcArchive.lookupString(stringTable, this.nameOffset);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%x@[name=%s,id=0x%x,hash=%d,type=0x%x,nameOffset=0x%x,fileOffset=0x%x,fileSize=0x%x]", hashCode(), name, id, hash, type, nameOffset, fileOffset, fileSize);
+  }
+}
---
+++ GCNCommon/src/net/medsouz/gcn/archive/rarc/RarcNode.java
@@ -0,0 +1,34 @@
+package net.medsouz.gcn.archive.rarc;
+
+import java.nio.ByteBuffer;
+
+public class RarcNode {
+  public static final int SIZE = 16;
+
+  public int id;
+  public int nameOffset;
+  public short hash;
+  public short files;
+  public int fileOffset;
+
+  public String name;
+
+  public RarcEntry[] entries;
+
+  public void read(ByteBuffer buf, ByteBuffer stringTable) {
+    this.id = buf.getInt();
+    this.nameOffset = buf.getInt();
+    this.hash = buf.getShort();
+    this.files = buf.getShort();
+    this.fileOffset = buf.getShort();
+    this.name = RarcArchive.lookupString(stringTable, this.nameOffset);
+
+    this.entries = new RarcEntry[this.files];
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%x@[name=%s,id=%s,nameOffset=0x%x,hash=0x%x,files=%d,fileOffset=0x%x]", hashCode(), name, new String(ByteBuffer.allocate(4).putInt(id).array()), nameOffset, hash, files, fileOffset);
+  }
+
+}
